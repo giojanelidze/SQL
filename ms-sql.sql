6b9d63f9342abaf37c96e4a5e5542912187b4884
giorgi <gio.janelidze88@gmail.com>
2024-06-12 14:07:33 +0000

    final commit

--- ms-sql.sql
+++ ms-sql.sql
@@ -45,6 +45,30 @@ GO
 
 
 /*******************************************/
+/* Create a scalar function that takes "x" and "y" parameters and calculates the area of the rectangle. Call the function three times with the following arguments: 5,7; 3,6; 7,9; */
 
+CREATE FUNCTION dbo.xyProduct (@x FLOAT, @y FLOAT)
+RETURNS FLOAT
+AS
+BEGIN
+    RETURN @x * @y;
+END;
+GO
 
+SELECT dbo.xyProduct(5, 7) AS xy1;
+SELECT dbo.xyProduct(3, 6) AS xy2;
+SELECT dbo.xyProduct(7, 9) AS xy3;
 
+/*******************************************/
+/*Write an SQL query that deletes records from the "students" table where the second letter in the "firstname" field is "a".*/
+-- პასუხი
+
+DELETE FROM students WHERE
+SUBSTRING(firstname, 2, 1) = 'a'
+
+/*******************************************/
+-- Update students table
+UPDATE dbo.students 
+    set firstname = 'girogi', 
+        lastname = 'janelidze'
+where student_ID = 1
--- ms-sql.ts
+++
@@ -1,8 +0,0 @@
-CREATE VIEW StudentsPayments AS
-
-SELECT s. firstname, s. lastname, p.amount, p. date
-FROM students s
-JOIN payments p
-ON s. student_id =p.student_id;
-
-SELECT * FROM StudentsPayments;
