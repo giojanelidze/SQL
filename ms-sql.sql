ece65e4e3e1a6dfdd024a83bb76fec1ed471c861
giorgi <gio.janelidze88@gmail.com>
2024-06-13 09:02:14 +0000

    file change

--- ms-sql.sql
+++ ms-sql.sql
@@ -1,74 +1,44 @@
-USE  TEST_DB
-GO
-CREATE  TABLE dbo.students (
-    student_ID integer primary key identity(1,1) ,
-    firstname nvarchar(100),
-    lastname nvarchar(100)
-)
+-- Open SQL Server Management Studio (SSMS):
 
-INSERT INTO STUDENTS(firstname, lastname) VALUES ( 'Clark', 'Sales');
-INSERT INTO STUDENTS( firstname, lastname) VALUES ( 'Dave', 'Accounting');
-INSERT INTO STUDENTS( firstname, lastname) VALUES ( 'Ava', 'tsandishvili');
+-- Launch SSMS and connect to your SQL Server instance.
+-- Connect to the Database Engine:
 
-SELECT * FROM STUDENTS;
+-- In the Object Explorer, connect to the database engine where you want to restore the backup.
+-- Restore the Database:
 
+-- Right-click on the "Databases" node and choose "Restore Database...".
+-- Specify Source and Destination:
 
-/*******************************************/
-USE  TEST_DB
-GO
-create table dbo.payments (
-    payment_ID int primary key identity(1,1),
-    student_ID int not null,
-    amount float,
-    date datetime
-);
-insert into payments (student_ID,amount, date) values (1, 2500, '2024-05-10');
-insert into payments (student_ID,  amount, date) values (2, 7500, '2024-05-20');
-insert into payments (student_ID, amount, date) values ( 3, 3000, '2024-05-15');
-select * from payments;
+-- In the "Restore Database" dialog, select "Device" as the source.
+-- Click the "..." button next to the "Device" field.
+-- Add Backup File:
 
-/*******************************************/
+-- In the "Select backup devices" dialog, click "Add".
+-- Navigate to the location of your .bak file, select it, and click "OK".
+-- Restore Database Options:
 
-USE  TEST_DB
-GO
-CREATE VIEW StudentsPayments AS
+-- Back in the "Restore Database" dialog, the backup file will appear in the "Backup sets to restore" grid. Ensure it is checked.
+-- In the "Destination" section, provide the name of the database to restore to. If you are restoring over an existing database, make sure the database is not currently in use.
+-- Options:
 
-SELECT s.firstname, s. lastname, p.amount, p. date
-FROM students s
-JOIN payments p
-ON s.student_ID =p.student_ID
-go
+-- Click on the "Options" page on the left side.
+-- Check "Overwrite the existing database (WITH REPLACE)" if you are restoring over an existing database.
+-- Ensure the file paths in the "Restore As" column are correct. Adjust them if necessary to point to the correct data and log file locations on your server.
+-- Execute Restore:
 
-SELECT * FROM StudentsPayments;
-GO
+-- Click "OK" to start the restore process. SSMS will restore the database from the .bak file.
+-- Here is an example of how you might perform the restore using T-SQL commands instead of SSMS:
 
+-- sql
+-- Copy code
+RESTORE DATABASE YourDatabaseName
+FROM DISK = 'C:\path\to\your\backupfile.bak'
+WITH REPLACE,
+     MOVE 'YourDatabaseName_Data' TO 'C:\path\to\your\datafile.mdf',
+     MOVE 'YourDatabaseName_Log' TO 'C:\path\to\your\logfile.ldf';
+-- In this script:
 
-
-/*******************************************/
-/* Create a scalar function that takes "x" and "y" parameters and calculates the area of the rectangle. Call the function three times with the following arguments: 5,7; 3,6; 7,9; */
-
-CREATE FUNCTION dbo.xyProduct (@x FLOAT, @y FLOAT)
-RETURNS FLOAT
-AS
-BEGIN
-    RETURN @x * @y;
-END;
-GO
-
-SELECT dbo.xyProduct(5, 7) AS xy1;
-SELECT dbo.xyProduct(3, 6) AS xy2;
-SELECT dbo.xyProduct(7, 9) AS xy3;
-
-/*******************************************/
-/*Write an SQL query that deletes records from the "students" table where the second letter in the "firstname" field is "a".*/
--- პასუხი
-
-DELETE FROM students WHERE
-SUBSTRING(firstname, 2, 1) = 'a'
-
-/*******************************************/
--- Update students table
-UPDATE dbo.students 
-    set firstname = 'girogi', 
-        lastname = 'janelidze'
-where student_ID = 1
+-- Replace YourDatabaseName with the name of the database you want to restore.
+-- Replace the path in DISK with the full path to your .bak file.
+-- Adjust the paths in the MOVE options to the correct locations for your data and log files.
+-- By following these steps or using the provided T-SQL script, you can successfully upload and restore a .bak file to your SQL Server database.
